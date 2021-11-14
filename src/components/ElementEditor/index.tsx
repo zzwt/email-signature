bee75cf8d1d72ba8c3262c4c741cb38edbfa1acd
zzwt <loophone@gmail.com>
2021-11-14 14:31:20 +1100

    Add animation of expandable field editor

--- src/components/ElementEditor/index.tsx
+++ src/components/ElementEditor/index.tsx
@@ -2,6 +2,8 @@ import React, { useCallback, useState } from 'react';
 import { BsArrowUp } from '@react-icons/all-files/bs/BsArrowUp';
 import { CgMoreO } from '@react-icons/all-files/cg/CgMoreO';
 import classNames from 'classnames';
+import { useSpring, animated } from 'react-spring';
+import useMeasure from 'react-use-measure';
 import styles from './styles.module.scss';
 import ColorEditor from '../ColorEditor';
 import Toggler from '../Toggler';
@@ -42,6 +44,10 @@ const ElementEditor: React.FC<ElementEditorProps> = ({
       ),
     [toggleMore]
   );
+
+  const [ref, { height }] = useMeasure();
+  const propps = useSpring({ height });
+
   return (
     <div className={styles.field_container}>
       <div className={styles.basic}>
@@ -64,153 +70,161 @@ const ElementEditor: React.FC<ElementEditorProps> = ({
           <div className={styles.toggler}>{renderToggler()}</div>
         </div>
       </div>
-      {toggleMore && (
-        <div className={styles.field_more}>
-          <div className={styles.field_more_label}>
-            <div className={styles.field_more_heading}>Label Settings</div>
-            {label.display.editable && (
-              <div className={styles.container_space}>
-                <Toggler
-                  label="Display"
-                  checked={label.display.value}
-                  onChange={(val) => {
-                    update(key, 'label', {
-                      ...label,
-                      display: { ...label.display, value: val },
-                    });
-                  }}
-                />
+      {
+        <animated.div className={styles.field_more} style={{ ...propps }}>
+          <div
+            className={classNames(styles.field_more_wrapper, {
+              [styles.field_more_wrapper_expanded]: toggleMore,
+            })}
+            ref={ref}
+          >
+            <div className={styles.field_more_label}>
+              <div className={styles.field_more_wrapper_heading}>
+                Label Settings
               </div>
-            )}
-            {label.type.editable && label.display.value && (
-              <div className={styles.container_space}>
-                <Input
-                  label="Label"
-                  value={label.value}
-                  onChange={(val) =>
-                    update(key, 'label', {
-                      ...label,
-                      value: val,
-                    })
-                  }
-                />
-              </div>
-            )}
-            {label.color.editable && label.display.value && (
-              <div className={styles.container_space}>
-                <ColorEditor
-                  color={label.color.value || meta.text}
-                  onColorChange={(color) => {
-                    update(key, 'label', {
-                      ...label,
-                      color: { ...label.color, value: color },
-                    });
-                  }}
-                />
-              </div>
-            )}
-            {label.size.editable && label.display.value && (
-              <div className={styles.container_space}>
-                <Slider
-                  label="Font Size"
-                  min={8}
-                  max={25}
-                  value={label.size.value}
-                  onChange={(val) => {
-                    update(key, 'label', {
-                      ...label,
-                      size: { ...label.size, value: val },
-                    });
-                  }}
-                />
-              </div>
-            )}
-            {label.bold.editable && label.display.value && (
-              <div className={styles.container_space}>
-                <Toggler
-                  label="Bold"
-                  checked={label.bold.value}
-                  onChange={(val) => {
-                    update(key, 'label', {
-                      ...label,
-                      bold: { ...label.bold, value: val },
-                    });
-                  }}
-                />
-              </div>
-            )}
-          </div>
-          <div className={styles.field_more_content}>
-            <div className={styles.field_more_heading}>
-              <span>Content Settings</span>
-              <button
-                className={styles.btn}
-                onClick={() => setDefaultFields(key)}
-              >
-                Reset
-              </button>
+              {label.display.editable && (
+                <div className={styles.container_space}>
+                  <Toggler
+                    label="Display"
+                    checked={label.display.value}
+                    onChange={(val) => {
+                      update(key, 'label', {
+                        ...label,
+                        display: { ...label.display, value: val },
+                      });
+                    }}
+                  />
+                </div>
+              )}
+              {label.type.editable && label.display.value && (
+                <div className={styles.container_space}>
+                  <Input
+                    label="Label"
+                    value={label.value}
+                    onChange={(val) =>
+                      update(key, 'label', {
+                        ...label,
+                        value: val,
+                      })
+                    }
+                  />
+                </div>
+              )}
+              {label.color.editable && label.display.value && (
+                <div className={styles.container_space}>
+                  <ColorEditor
+                    color={label.color.value || meta.text}
+                    onColorChange={(color) => {
+                      update(key, 'label', {
+                        ...label,
+                        color: { ...label.color, value: color },
+                      });
+                    }}
+                  />
+                </div>
+              )}
+              {label.size.editable && label.display.value && (
+                <div className={styles.container_space}>
+                  <Slider
+                    label="Font Size"
+                    min={8}
+                    max={25}
+                    value={label.size.value}
+                    onChange={(val) => {
+                      update(key, 'label', {
+                        ...label,
+                        size: { ...label.size, value: val },
+                      });
+                    }}
+                  />
+                </div>
+              )}
+              {label.bold.editable && label.display.value && (
+                <div className={styles.container_space}>
+                  <Toggler
+                    label="Bold"
+                    checked={label.bold.value}
+                    onChange={(val) => {
+                      update(key, 'label', {
+                        ...label,
+                        bold: { ...label.bold, value: val },
+                      });
+                    }}
+                  />
+                </div>
+              )}
             </div>
+            <div className={styles.field_more_content}>
+              <div className={styles.field_more_wrapper_heading}>
+                <span>Content Settings</span>
+                <button
+                  className={styles.btn}
+                  onClick={() => setDefaultFields(key)}
+                >
+                  Reset
+                </button>
+              </div>
 
-            {content.type.editable && (
-              <div className={styles.container_space}>
-                <TypeSelector
-                  label="Type"
-                  // options={['Text', 'Link', 'Email']}
-                  type={ContentType}
-                  active={content.type.value}
-                  onChange={(val) => {
-                    update(key, 'content', {
-                      ...content,
-                      type: { ...content.type, value: val },
-                    });
-                  }}
-                />
-              </div>
-            )}
-            {content.color.editable && (
-              <div className={styles.container_space}>
-                <ColorEditor
-                  color={content.color.value || meta.text}
-                  onColorChange={(color) => {
-                    update(key, 'content', {
-                      ...content,
-                      color: { ...content.color, value: color },
-                    });
-                  }}
-                />
-              </div>
-            )}
-            {content.size.editable && (
-              <div className={styles.container_space}>
-                <Slider
-                  label="Font Size"
-                  min={8}
-                  max={25}
-                  value={content.size.value}
-                  onChange={(val) => {
-                    update(key, 'content', {
-                      ...content,
-                      size: { ...content.size, value: val },
-                    });
-                  }}
-                />
-              </div>
-            )}
-            {content.bold.editable && (
-              <div className={styles.container_space}>
-                <Toggler
-                  label="Bold"
-                  checked={content.bold.value}
-                  onChange={(val) => {
-                    update(key, 'content', {
-                      ...content,
-                      bold: { ...content.bold, value: val },
-                    });
-                  }}
-                />
-              </div>
-            )}
-            {/* <div
+              {content.type.editable && (
+                <div className={styles.container_space}>
+                  <TypeSelector
+                    label="Type"
+                    // options={['Text', 'Link', 'Email']}
+                    type={ContentType}
+                    active={content.type.value}
+                    onChange={(val) => {
+                      update(key, 'content', {
+                        ...content,
+                        type: { ...content.type, value: val },
+                      });
+                    }}
+                  />
+                </div>
+              )}
+              {content.color.editable && (
+                <div className={styles.container_space}>
+                  <ColorEditor
+                    color={content.color.value || meta.text}
+                    onColorChange={(color) => {
+                      update(key, 'content', {
+                        ...content,
+                        color: { ...content.color, value: color },
+                      });
+                    }}
+                  />
+                </div>
+              )}
+              {content.size.editable && (
+                <div className={styles.container_space}>
+                  <Slider
+                    label="Font Size"
+                    min={8}
+                    max={25}
+                    value={content.size.value}
+                    onChange={(val) => {
+                      update(key, 'content', {
+                        ...content,
+                        size: { ...content.size, value: val },
+                      });
+                    }}
+                  />
+                </div>
+              )}
+              {content.bold.editable && (
+                <div className={styles.container_space}>
+                  <Toggler
+                    label="Bold"
+                    checked={content.bold.value}
+                    onChange={(val) => {
+                      update(key, 'content', {
+                        ...content,
+                        bold: { ...content.bold, value: val },
+                      });
+                    }}
+                  />
+                </div>
+              )}
+              {/* <div
               className={classNames(
                 styles.container_space,
                 styles.field_more_content_default
@@ -218,9 +232,10 @@ const ElementEditor: React.FC<ElementEditorProps> = ({
             >
               
             </div> */}
+            </div>
           </div>
-        </div>
-      )}
+        </animated.div>
+      }
     </div>
   );
 };
